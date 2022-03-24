2ee5d03e06f64d1bf2fe6204a6e2dcc1e6f05c6c
nogira <83847986+nogira@users.noreply.github.com>
2022-03-25 00:30:46 +1100

    Update i.sh

--- i.sh
+++ i.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y build-essential cmake wget pkg-config clang \
     libclang-dev llvm-dev libudev-dev libfreetype6-dev libexpat1-dev
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 git clone https://github.com/darkrenaissance/darkfi
-cd darkfi
+cd darkfi/
 make
 sudo make install
 mkdir -p ~/.config/darkfi
