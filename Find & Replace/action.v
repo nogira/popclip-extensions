a664660be0f6d8ce72ac6d2b8d0698da7af5d23a
nogira <83847986+nogira@users.noreply.github.com>
2022-04-16 02:08:49 +1000

    find & replace now supports regex

--- Find & Replace/action.v
+++ Find & Replace/action.v
@@ -6,6 +6,7 @@ mut:
 	input                  string
 	find                   string
 	replace                string
+	is_regex               bool
 	soft_input_visible     bool
 	soft_input_buffer      string
 	soft_input_parsed_char string
@@ -14,19 +15,22 @@ mut:
 
 fn main() {
 	mut app := &App{
-		input: os.getenv("POPCLIP_FULL_TEXT")
+		input: 'this is a sentence\nthis is a sentence'//os.getenv("POPCLIP_FULL_TEXT")
 		find : ''
 		replace : ''
+		is_regex : true
 	}
 	
 	c := ui.column(
 		widths: ui.stretch
-		heights: [ui.compact, ui.stretch]
+		// heights: [ui.compact, ui.stretch]
+		heights: ui.stretch
 		margin_: 5
 		spacing: 10
 		children: [
 			ui.row(
 				spacing: 5
+				height: 30
 				children: [
 					ui.label(
 						text: 'Find' //&app.tb
@@ -35,6 +39,7 @@ fn main() {
 					ui.textbox(
 						id: 'tb1'
 						width: 180
+						// height: 30
 						//mode: .multiline | .word_wrap
 						text: &app.find
 						// fitted_height: true
@@ -57,17 +62,33 @@ fn main() {
 					),
 				]
 			),
-			ui.button(
-				text: 'Ok'
-				onclick: btn_find_replace_click
+			ui.row(
+				spacing: 5
+				children: [
+					// ui.label(
+					// 	text: 'Regex' //&app.tb
+					// 	width: 50
+					// ),
+					ui.checkbox(
+						checked: &app.is_regex
+						text: 'Regex'
+					),
+					ui.spacing(width: 10),
+					ui.button(
+						height: 20
+						text: 'Replace All'
+						onclick: btn_find_replace_click
+					),
+				]
 			),
+			
 			
 		]
 	)
 	w := ui.window(
 		state: app
-		width: 250
-		height: 100
+		width: 245
+		height: 120
 		mode: .resizable
 		// on_init: init
 		children: [c]
@@ -78,11 +99,18 @@ fn main() {
 
 fn btn_find_replace_click(mut app App, x voidptr) {
 	// replace find with replace on input
-	find := app.find.replace('\\n', '\n')
-					.replace('\\t', '\t')
-	replace := app.replace.replace('\\n', '\n')
-						.replace('\\t', '\t')
-	output := app.input.replace(find, replace)
-	print(output)
+	find := app.find
+	replace := app.replace
+	if app.is_regex {
+		// use javascript regex bc its more known
+		jsc_path := "/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Helpers/jsc"
+		cmd := "'$jsc_path' -e 'print(`$app.input`.replace(/$find/gm,`$replace`))'"
+		output := os.execute(cmd).output
+		// remove newline
+		print(output.substr(0, output.len - 1))
+	} else {
+		output := app.input.replace(find, replace)
+		print(output)
+	}
 	exit(0)
 }
